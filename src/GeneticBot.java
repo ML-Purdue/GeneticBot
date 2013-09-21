fc819ded8b43c561085c0e16afbfb6f3cf81511a
GREGG A WEAVER <weaverg@sslab14.cs.purdue.edu>
2013-09-21 16:37:26 -0400

    Added debug output, randomRange, and shouldFire
    debug prints a set of weight equations and inputs
    randomRange generates a random number in the specified range
    shouldFire predicates the firing of a bullet based on its weight equation

--- src/GeneticBot.java
+++ src/GeneticBot.java
@@ -16,41 +16,55 @@ public class GeneticBot extends AdvancedRobot {
     public void run() {
         ArrayList<Double> turnGunEquationWeights = new ArrayList<Double>();
         ArrayList<Double> fireEquationWeights = new ArrayList<Double>();
-
+        ArrayList<Double> shouldFireEquationWeights = new ArrayList<Double>();
+        
         ArrayList<Double> inputs = new ArrayList<Double>();
-
+        
         inputs.add((double) 1); // constant factor
-        turnGunEquationWeights.add(Math.random());
-        fireEquationWeights.add(Math.random());
-
+        turnGunEquationWeights.add(randomRange(-1, 1));
+        fireEquationWeights.add(randomRange(-1, 1));
+        shouldFireEquationWeights.add(randomRange(-1, 1));
+        
         inputs.add((double) 0); // time since last scan
-        turnGunEquationWeights.add(Math.random());
-        fireEquationWeights.add(Math.random());
-
+        turnGunEquationWeights.add(randomRange(-1, 1));
+        fireEquationWeights.add(randomRange(-1, 1));
+        shouldFireEquationWeights.add(randomRange(-1, 1));
+        
         while (true) {
             if (lastScanEvent != null) {
                 inputs.set(1, (double) (getTime() - lastScanEvent.getTime()));
             }
             double turnGun = evaluateEquation(turnGunEquationWeights, inputs);
+            boolean shouldFire = evaluateEquation(shouldFireEquationWeights, inputs) > 0;
             double fire = evaluateEquation(fireEquationWeights, inputs);
             setTurnGunRight(turnGun); // schedule the gun to turn
-            setFire(fire); // schedule to fire a bullet
-            System.out.printf("turnGun %.2f = %.2f*%.2f + %.2f*%.2f, fire %.2f = %.2f*%.2f + %.2f*%.2f\n",
-                    turnGun,
-                    turnGunEquationWeights.get(0),
-                    inputs.get(0),
-                    turnGunEquationWeights.get(1),
-                    inputs.get(1),
-                    fire,
-                    fireEquationWeights.get(0),
-                    inputs.get(0),
-                    fireEquationWeights.get(1),
-                    inputs.get(1));
+            if (shouldFire) {
+            	setFire(fire); // schedule to fire a bullet
+            }
+            System.out.println("Turn " + getTime());
+            debug("turnGun", turnGun, turnGunEquationWeights, inputs);
+            debug("shouldFire", shouldFire ? 1 : 0, shouldFireEquationWeights, inputs);
+            debug("fire", fire, fireEquationWeights, inputs);
+            
             execute(); // give Robocode control for one game tick
         }
     }
 
-    // Evaluate a linear equation in the form of [value = weight1 * input1 + weight2* input2 + w3 * i3 + ...]
+    private void debug(String name, double value, ArrayList<Double> weights, ArrayList<Double> inputs) {
+    	if (weights.size() == 0) {
+    		return;
+    	}
+    	
+    	String expressionFormatter = " %.2f*%.2f";
+    	String rightHandSide = String.format("%.2f*%.2f", weights.get(0), inputs.get(0));
+    	for (int i = 1; i < weights.size(); i++) {
+    		rightHandSide += " + " + String.format(expressionFormatter, weights.get(i), inputs.get(i));
+    	}
+    	
+    	System.out.printf("%s %.2f = %s\n", name, value, rightHandSide);
+	}
+
+	// Evaluate a linear equation in the form of [value = weight1 * input1 + weight2* input2 + w3 * i3 + ...]
     private double evaluateEquation(ArrayList<Double> weights, ArrayList<Double> inputs) {
         double value = 0;
         for (int i = 0; i < weights.size(); i++) {
@@ -61,5 +75,9 @@ public class GeneticBot extends AdvancedRobot {
 
     public void onScannedRobot(ScannedRobotEvent e) {
         lastScanEvent = e; // save the last scanned robot event for input into equations
+    }
+    
+    private double randomRange(double a, double b) {
+    	return a + Math.random() * (b-a);
     }
 }
